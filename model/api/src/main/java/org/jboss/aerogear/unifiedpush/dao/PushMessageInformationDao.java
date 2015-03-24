93329fe508bbcf539442d69d1f33a8ed0abda819
sebastienblanc <scm.blanc@gmail.com>
2015-03-24 09:28:15 +0100

    pass metric id to the flow

--- model/api/src/main/java/org/jboss/aerogear/unifiedpush/dao/PushMessageInformationDao.java
+++ model/api/src/main/java/org/jboss/aerogear/unifiedpush/dao/PushMessageInformationDao.java
@@ -80,4 +80,6 @@ public interface PushMessageInformationDao extends GenericBaseDao<PushMessageInf
     List<String> findVariantIDsWithWarnings();
     List<PushMessageInformation> findLatestActivity(int maxResults);
     long getNumberOfPushMessagesForApplications();
+
+    PushMessageInformation getPushMessageInformation(String id);
 }
